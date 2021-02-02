03ea13f6e046f12295694d4566da2c14daf25b3e
chris kim <chriskim0926@gmail.com>
2021-02-01 23:26:21 -0500

    update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -23,9 +23,9 @@ const navbar = () => {
                 {/* <li class="nav-item"> <NavLink exact to="/about" className="nav-link" activeClassName= "nav-link active"> About </NavLink></li>
                 <li class="nav-item"> <NavLink exact to="/portfolio" className="nav-link" activeClassName= "nav-link active"> Portfolio </NavLink></li>
                 <li class="nav-item"> <NavLink exact to="/contact" className="nav-link" activeClassName= "nav-link active"> Contact </NavLink></li> */}
-                <li className="nav-item">
+                <li class ="nav-item">
             <NavLink
-              to="/about"
+              to="/about" class="nav-link js-scroll-trigger"
             >
               About
             </NavLink>
